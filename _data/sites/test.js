a541cd693b990a2fdebfca1ca727f1665038e087
Rob Slippey <rwslippey@gmail.com>
2022-12-29 13:53:44 -0500

    Create test.js

---
+++ _data/sites/test.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+	name: "My Tests", // optional, falls back to object key
+	description: "The default sites that get tested",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://robslippey.com"
+	]
+};
