94c630b7ada98b2cc042fdf2455bbae4d3eb9356
Rob Slippey <rwslippey@gmail.com>
2022-12-29 16:02:57 -0500

    Updates sites

--- _data/sites/websites.js
+++ _data/sites/websites.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-	name: "SSG Sites", // optional, falls back to object key
-	description: "Static sites",
+	name: "Websites", // optional, falls back to object key
+	description: "Websites",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
